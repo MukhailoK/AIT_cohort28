9df99105ada57fb8a5a8a900df2f089a0763ff7b
MukhailoK <96546140+MukhailoK@users.noreply.github.com>
2023-06-06 14:22:41 +0200

    Lesson 13 homework

--- homework/lesson_13/src/SquareXApp.java
+++ homework/lesson_13/src/SquareXApp.java
@@ -2,9 +2,9 @@ import model.SquareX;
 
 public class SquareXApp {
     public static void main(String[] args) {
-        SquareX squareX1 = new SquareX(1, -8, 15);
-        SquareX squareX2 = new SquareX(1,2,3);
-        SquareX squareX3 = new SquareX(2,-8,8);
+        SquareX squareX1 = new SquareX(1, -8, 15); // Discriminant>0
+        SquareX squareX2 = new SquareX(2, -8, 8); //Discriminant==0
+        SquareX squareX3 = new SquareX(1, 2, 3); //Discriminant<0
         System.out.println(squareX1.getResult());
         System.out.println(squareX2.getResult());
         System.out.println(squareX3.getResult());
