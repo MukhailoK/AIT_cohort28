b258b12fe36fc944c2ca6da162572df40cc76403
MukhailoK <96546140+MukhailoK@users.noreply.github.com>
2023-06-19 23:29:10 +0200

    homework lesson 22

--- homework/lesson_22/src/model/User.java
+++ homework/lesson_22/src/model/User.java
@@ -83,46 +83,46 @@ public class User {
 //=============================================================
 
     //V2
-    //  private boolean validatePassword(String password) {
+    private boolean validatePassword(String password) {
 
-    //        boolean validUpper = false;
-//        boolean validLower = false;
-//        boolean validDigit = false;
-//        boolean validSpecSymb = false;
-//
-//        if (password.length() < 8) {
-//            return false;
-//        }
-//        for (int i = 0; i < password.length(); i++) {
-//            if (!validUpper) {
-//                validUpper = password.charAt(i) > 47 && password.charAt(i) < 58;
-//            }
-//            if (!validLower) {
-//                validLower = password.charAt(i) > 96 && password.charAt(i) < 123;
-//            }
-//            if (!validDigit) {
-//                validDigit = password.charAt(i) > 64 && password.charAt(i) < 91;
-//            }
-//            if (!validSpecSymb) {
-//                validSpecSymb = (password.charAt(i) == '!'
-//                        || password.charAt(i) == '%'
-//                        || password.charAt(i) == '@'
-//                        || password.charAt(i) == '*'
-//                        || password.charAt(i) == '&');
-//            }
-//    }
-//        return validUpper == validDigit == validLower == validSpecSymb;
-//
+        boolean validUpper = false;
+        boolean validLower = false;
+        boolean validDigit = false;
+        boolean validSpecSymb = false;
+
+        if (password.length() < 8) {
+            return false;
+        }
+        for (int i = 0; i < password.length(); i++) {
+            if (!validUpper) {
+                validUpper = password.charAt(i) > 47 && password.charAt(i) < 58;
+            }
+            if (!validLower) {
+                validLower = password.charAt(i) > 96 && password.charAt(i) < 123;
+            }
+            if (!validDigit) {
+                validDigit = password.charAt(i) > 64 && password.charAt(i) < 91;
+            }
+            if (!validSpecSymb) {
+                validSpecSymb = (password.charAt(i) == '!'
+                        || password.charAt(i) == '%'
+                        || password.charAt(i) == '@'
+                        || password.charAt(i) == '*'
+                        || password.charAt(i) == '&');
+            }
+        }
+        return validUpper == validDigit == validLower == validSpecSymb;
+    }
     //==================================================================
     //V3
-    private boolean validatePassword(String password) {
-        return checkLength(password)
-                && haseUpperSymb(password)
-                && haseLowerSymb(password)
-                && haseDigitSymb(password)
-                && haseSpecialSymb(password);
-
-    }
+//    private boolean validatePassword(String password) {
+//        return checkLength(password)
+//                && haseUpperSymb(password)
+//                && haseLowerSymb(password)
+//                && haseDigitSymb(password)
+//                && haseSpecialSymb(password);
+//
+//    }
 
     private boolean checkLength(String password) {
         return password.length() >= 8;
