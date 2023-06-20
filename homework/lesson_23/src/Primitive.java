fc40aef1b79ec66c9a251e5b6caea5a4b9ef8652
MukhailoK <96546140+MukhailoK@users.noreply.github.com>
2023-06-20 14:06:16 +0200

    homework lesson 23

--- homework/lesson_23/src/Primitive.java
+++ homework/lesson_23/src/Primitive.java
@@ -8,26 +8,48 @@ public class Primitive {
 
     public static String checkPimitive(String[] args) {
         String res = "";
+        int match = 0;
         for (String a : args) {
             switch (a) {
-                case "byte" -> res += "byte MIN_VALUE = " + Byte.MIN_VALUE
-                        + "\nbyte MAX_VALUE = " + Byte.MAX_VALUE;
-                case "int" -> res += "\nint MIN_VALUE = " + Integer.MIN_VALUE
-                        + "\nint MAX_VALUE = " + Integer.MAX_VALUE;
-                case "short" -> res += "\nshort MIN_VALUE = " + Short.MIN_VALUE
-                        + "\nshort MAX_VALUE = " + Short.MAX_VALUE;
-                case "long" -> res += "\nlong MIN_VALUE = " + Long.MIN_VALUE
-                        + "\nlong MAX_VALUE = " + Long.MAX_VALUE;
-                case "char" -> res += "\nchar MIN_VALUE = " + Character.MIN_VALUE
-                        + "\nchar MAX_VALUE = " + Character.MAX_VALUE;
-                case "float" -> res += "\nfloat MIN_VALUE = " + Float.MIN_VALUE
-                        + "\nfloat MAX_VALUE = " + Float.MAX_VALUE;
-                case "boolean" -> res += "\nboolean MIN_VALUE = " + Boolean.FALSE
-                        + "\nboolean MAX_VALUE = " + Boolean.TRUE;
+                case "byte" -> {
+                    res += "byte MIN_VALUE = " + Byte.MIN_VALUE
+                            + "\nbyte MAX_VALUE = " + Byte.MAX_VALUE;
+                    match++;
+                }
+                case "int" -> {
+                    res += "\nint MIN_VALUE = " + Integer.MIN_VALUE
+                            + "\nint MAX_VALUE = " + Integer.MAX_VALUE;
+                    match++;
+                }
+                case "short" -> {
+                    res += "\nshort MIN_VALUE = " + Short.MIN_VALUE
+                            + "\nshort MAX_VALUE = " + Short.MAX_VALUE;
+                    match++;
+                }
+                case "long" -> {
+                    res += "\nlong MIN_VALUE = " + Long.MIN_VALUE
+                            + "\nlong MAX_VALUE = " + Long.MAX_VALUE;
+                    match++;
+                }
+                case "char" -> {
+                    res += "\nchar MIN_VALUE = " + Character.MIN_VALUE
+                            + "\nchar MAX_VALUE = " + Character.MAX_VALUE;
+                    match++;
+                }
+                case "float" -> {
+                    res += "\nfloat MIN_VALUE = " + Float.MIN_VALUE
+                            + "\nfloat MAX_VALUE = " + Float.MAX_VALUE;
+                    match++;
+                }
+                case "boolean" -> {
+                    res += "\nboolean MIN_VALUE = " + Boolean.FALSE
+                            + "\nboolean MAX_VALUE = " + Boolean.TRUE;
+                    match++;
+                }
                 default -> res += "\nWrong argument : " + a;
             }
         }
-        if (res.length() == 0) {
+        if (match == 0) {
             return "byte MIN_VALUE = " + Byte.MIN_VALUE
                     + "\nbyte MAX_VALUE = " + Byte.MAX_VALUE
                     + "\nint MIN_VALUE = " + Integer.MIN_VALUE
