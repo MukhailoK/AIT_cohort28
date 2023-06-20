b6812e1fe50d5c6a26b1a7d52c939e7d955ba105
MukhailoK <96546140+MukhailoK@users.noreply.github.com>
2023-06-20 14:02:30 +0200

    homework lesson 23

--- homework/lesson_23/src/Primitive.java
+++ homework/lesson_23/src/Primitive.java
@@ -9,33 +9,22 @@ public class Primitive {
     public static String checkPimitive(String[] args) {
         String res = "";
         for (String a : args) {
-            if (a.equals("byte")) {
-                res += "byte MIN_VALUE = " + Byte.MIN_VALUE
+            switch (a) {
+                case "byte" -> res += "byte MIN_VALUE = " + Byte.MIN_VALUE
                         + "\nbyte MAX_VALUE = " + Byte.MAX_VALUE;
-            }
-            if (a.equals("int")) {
-                res += "\nint MIN_VALUE = " + Integer.MIN_VALUE
+                case "int" -> res += "\nint MIN_VALUE = " + Integer.MIN_VALUE
                         + "\nint MAX_VALUE = " + Integer.MAX_VALUE;
-            }
-            if (a.equals("short")) {
-                res += "\nshort MIN_VALUE = " + Short.MIN_VALUE
+                case "short" -> res += "\nshort MIN_VALUE = " + Short.MIN_VALUE
                         + "\nshort MAX_VALUE = " + Short.MAX_VALUE;
-            }
-            if (a.equals("long")) {
-                res += "\nlong MIN_VALUE = " + Long.MIN_VALUE
+                case "long" -> res += "\nlong MIN_VALUE = " + Long.MIN_VALUE
                         + "\nlong MAX_VALUE = " + Long.MAX_VALUE;
-            }
-            if (a.equals("char")) {
-                res += "\nchar MIN_VALUE = " + Character.MIN_VALUE
+                case "char" -> res += "\nchar MIN_VALUE = " + Character.MIN_VALUE
                         + "\nchar MAX_VALUE = " + Character.MAX_VALUE;
-            }
-            if (a.equals("float")) {
-                res += "\nfloat MIN_VALUE = " + Float.MIN_VALUE
+                case "float" -> res += "\nfloat MIN_VALUE = " + Float.MIN_VALUE
                         + "\nfloat MAX_VALUE = " + Float.MAX_VALUE;
-            }
-            if (a.equals("boolean")) {
-                res += "\nboolean MIN_VALUE = " + Boolean.FALSE
+                case "boolean" -> res += "\nboolean MIN_VALUE = " + Boolean.FALSE
                         + "\nboolean MAX_VALUE = " + Boolean.TRUE;
+                default -> res += "\nWrong argument : " + a;
             }
         }
         if (res.length() == 0) {
