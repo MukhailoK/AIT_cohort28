e42ab2668bdc7c1d142b8fa2c411407e061ceab3
MukhailoK <96546140+MukhailoK@users.noreply.github.com>
2023-07-19 22:45:24 +0200

    homework lesson 44

--- basic_programming/homework/lesson_44/src/Main.java
+++ basic_programming/homework/lesson_44/src/Main.java
@@ -2,24 +2,20 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[100];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = i + 1;
-        }
-        Random random = new Random();
-        int rd = random.nextInt(1, 101);
-        int rdInd = random.nextInt(0, 100);
-        array[rdInd] = rd;
-        System.out.println("index of duplicate = "
-                + getIndexOfDuplicate(array)
-                + ", number = "
-                + array[getIndexOfDuplicate(array)]);
+        int[] array = initialArray(100);
+        System.out.println("Before");
+        printArr(array);
+        injectRandomElement(array);
+        System.out.println("After");
+        printArr(array);
+        getResult(array);
     }
 
     public static int getIndexOfDuplicate(int[] array) {
         int index = -1;
+        int sumFirstLast = array.length + 1;
         for (int i = 0, j = array.length - 1; i < array.length / 2; i++, j--) {
-            if (array[i] + array[j] != 101) {
+            if (array[i] + array[j] != sumFirstLast) {
                 if (array[i] != i + 1) {
                     index = i;
                 } else {
@@ -29,5 +25,38 @@ public class Main {
             }
         }
         return index;
+    }
+
+    public static void printArr(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void injectRandomElement(int[] array) {
+        Random random = new Random();
+        int rd = random.nextInt(1, 101);
+        int rdInd = random.nextInt(0, 100);
+        array[rdInd] = rd;
+    }
+
+    public static int[] initialArray(int size) {
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        return array;
+    }
+
+    public static void getResult(int[] array) {
+        int index = getIndexOfDuplicate(array);
+        if (index != -1) {
+            System.out.println("\nindex of duplicate = "
+                    + index
+                    + ", number = "
+                    + array[index]);
+        } else {
+            System.out.println("not found");
+        }
     }
 }
