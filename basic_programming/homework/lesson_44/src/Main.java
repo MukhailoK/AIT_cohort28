8174be0c5f1cd5c379972b3bec12a21606a0a6de
MukhailoK <96546140+MukhailoK@users.noreply.github.com>
2023-07-19 22:21:29 +0200

    homework lesson 44

--- basic_programming/homework/lesson_44/src/Main.java
+++ basic_programming/homework/lesson_44/src/Main.java
@@ -10,16 +10,22 @@ public class Main {
         int rd = random.nextInt(1, 101);
         int rdInd = random.nextInt(0, 100);
         array[rdInd] = rd;
-        System.out.println("index of duplicate = " + getIndexOfDuplicate(array));
+        System.out.println("index of duplicate = "
+                + getIndexOfDuplicate(array)
+                + ", number = "
+                + array[getIndexOfDuplicate(array)]);
     }
 
     public static int getIndexOfDuplicate(int[] array) {
         int index = -1;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] != i + 1) {
-                index = i;
+        for (int i = 0, j = array.length - 1; i < array.length / 2; i++, j--) {
+            if (array[i] + array[j] != 101) {
+                if (array[i] != i + 1) {
+                    index = i;
+                } else {
+                    index = j;
+                }
                 break;
-
             }
         }
         return index;
